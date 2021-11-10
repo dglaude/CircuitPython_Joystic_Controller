a9ef8855b9be457b25c9a436fcbbf6aebe39b4e9
David Glaude <dglaude@users.noreply.github.com>
2021-11-10 02:38:06 +0100

    Untested not enought mempory on M0 QTPy

--- examples/sneswii2gamepad/code.py
+++ examples/sneswii2gamepad/code.py
@@ -1,4 +1,10 @@
-### This pretend to use gamepad_controller but in fact it is just a copy of the radial controller
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# You must add a gamepad HID device inside your boot.py file
+# in order to use this example.
+# See this Learn Guide for details:
+# https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/hid-devices#custom-hid-devices-3096614-9
 
 import time
 import board
@@ -22,15 +28,15 @@ controller = ClassicController(board.I2C())
 # SPDX-License-Identifier: Unlicense
 
 import usb_hid
-###import adafruit_radial_controller
-import gamepad_controller
 
-###radial_controller = adafruit_radial_controller.RadialController(usb_hid.devices)
-gamepad_controller = gamepad_controller.GamepadController(usb_hid.devices)
+from hid_gamepad import Gamepad
 
-position = 0
-last_position = 0
-DEGREE_TENTHS_MULTIPLIER = 100
+gp = Gamepad(usb_hid.devices)
+
+x=0
+y=0
+oldx=0
+oldy=0
 
 while True:
     _, buttons, dpad, _ = controller.values
@@ -45,11 +51,9 @@ while True:
         led[0] = (0, 255, 0)
     if buttons.R:
         led[0] = (0, 0, 0)
-        position = position+1
         print("button R")
     if buttons.L:
         led[0] = (0, 0, 0)
-        position = position-1
         print("button L")
     if buttons.start:
         led[0] = (0, 0, 0)
@@ -57,20 +61,26 @@ while True:
     if buttons.select:
         led[0] = (0, 0, 0)
         print("button select")
+    if (y!=0) and not (dpad.up or dpad.down):
+        y=0
     if dpad.up:
+        y = 127
         led[0] = (0, 0, 0)
         print("dpad up")
     if dpad.down:
+        y = -127
         led[0] = (0, 0, 0)
         print("dpad down")
+    if (x!=0) and not (dpad.right or dpad.left):
+        x=0
     if dpad.right:
+        x = 127
         led[0] = (0, 0, 0)
         print("dpad right")
     if dpad.left:
+        x = -127
         led[0] = (0, 0, 0)
         print("dpad left")
 
-    delta = position - last_position
-    if delta != 0:
-        gamepad_controller.rotate(delta * DEGREE_TENTHS_MULTIPLIER)
-        last_position = position
+    gp.move_joysticks(x, y)
+
